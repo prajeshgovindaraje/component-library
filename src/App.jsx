4d07cda966b0096889c23112b69558dcf60ab07d
prajeshgovindaraje <prajeshgovindaraje@gmail.com>
2025-02-18 21:17:15 +0530

    badge component done

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+
+import Badge from './components/Badge'
+
+export default function App(){
+  return(
+
+
+    <section className="badge-section">
+        <h1>BADGES</h1>
+
+        <div className="badge-row">
+
+          <h1>SQUARE</h1>
+          <Badge color="gray" shape="square">Badge</Badge>
+          <Badge color="red" shape="square">Badge</Badge>
+          <Badge color="yellow" shape="square">Badge</Badge>
+          <Badge color="green" shape="square">Badge</Badge>
+          <Badge color="blue" shape="square">Badge</Badge>
+          <Badge color="purple" shape="square">Badge</Badge>
+          <Badge color="indigo" shape="square">Badge</Badge>
+          <Badge color="pink" shape="square">Badge</Badge>
+          
+
+        </div>
+        <div className="badge-row">
+
+          <h1>PILL</h1>
+          <Badge color="gray" shape="pill">Badge</Badge>
+          <Badge color="red" shape="pill">Badge</Badge>
+          <Badge color="yellow" shape="pill">Badge</Badge>
+          <Badge color="green" shape="pill">Badge</Badge>
+          <Badge color="blue" shape="pill">Badge</Badge>
+          <Badge color="purple" shape="pill">Badge</Badge>
+          <Badge color="indigo" shape="pill">Badge</Badge>
+          <Badge color="pink" shape="pill">Badge</Badge>
+
+        </div>
+    </section>
+
+
+  )
+}
---
+++ src/components/Badge.jsx
@@ -0,0 +1,19 @@
+
+import { clsx } from "clsx"
+
+export default function Badge({children,color,shape}){
+
+    
+
+ 
+
+    return(
+
+        <div className={`badge ${shape} ${color}`}>
+            {children}
+        </div>
+
+    )
+
+
+}
